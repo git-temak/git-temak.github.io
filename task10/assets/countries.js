2c6c71e3c73f313a2d6175bcd9ea9bc257425250
Temak <makindeoluwatimilehin@gmail.com>
2020-11-16 23:15:41 +0100

    add loading effect
    show a loader icon while the data is being loaded

--- task10/assets/countries.js
+++ task10/assets/countries.js
@@ -1,35 +1,42 @@
-const countryName = document.getElementById("country-name");
-const flag = document.getElementById("flag");
-const capital = document.getElementById("capital");
-const continent = document.getElementById("continent");
-const subRegion = document.getElementById("sub-region");
-const currency = document.getElementById("currency");
-const language = document.getElementById("language");
-const countrySection = document.querySelector(".country-info");
+const table = document.getElementById("country-table");
+const tableBody = document.getElementById("table-body");
 
-const tableRow = document.createElement('tr');
-const tableCell = document.createElement('td');
+const spinner = document.getElementById("spinner");
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	spinner.removeAttribute('hidden');
+
 	fetch('https://restcountries.eu/rest/v2/all')
 	.then(response => response.json())
-	.then(data => {
+	.then(data => {		
 		let values = Object.values(data);
-		values.forEach(country => {
-			tableCell.innerHTML = country.name;
-			tableRow.append(tableCell);
-			console.log(country.name);
-			// countryName.innerHTML = country.name;
-			// flag.src = country.flag;
-			// capital.innerHTML = country.capital;
-			// continent.innerHTML = country.region;
-			// subRegion.innerHTML = country.subregion;
-			// currency.innerHTML = `${country.currencies[0].name} (${country.currencies[0].symbol})`;
-			// const langs = [];
-			// country.languages.forEach(lang => langs.push(lang.name));
-			// language.innerHTML = langs;
+		values.reverse().forEach(country => {
+			const row = tableBody.insertRow(0);
+			const cell1 = row.insertCell(0);
+			const cell2 = row.insertCell(1);
+			const cell3 = row.insertCell(2);
+			const cell4 = row.insertCell(3);
+			const cell5 = row.insertCell(4);
+			const cell6 = row.insertCell(5);
+			const cell7 = row.insertCell(6);
+
+			const langs = [];
+			country.languages.forEach(lang => langs.push(lang.name));
+
+			cell1.innerHTML = `<img src="${country.flag}" alt="">`;
+			cell2.innerHTML = country.name;
+			cell3.innerHTML = country.capital;
+			cell4.innerHTML = country.region;
+			cell5.innerHTML = country.subregion;
+			cell6.innerHTML = `${country.currencies[0].name} (${country.currencies[0].symbol})`;
+			cell7.innerHTML = langs;
 		})
+
+		setTimeout(() => {
+			spinner.setAttribute('hidden', '')
+			table.removeAttribute('hidden', '')
+		}, 1000);
 	})
 	.catch(error => console.log(error))
 
