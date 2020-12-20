b07ca46847c3c4f62975f332da3da203de6eb5be
Temak <makindeoluwatimilehin@gmail.com>
2020-12-20 13:22:10 +0100

    login functionality added

--- jsminna/store-app/assets/app.js
+++ jsminna/store-app/assets/app.js
@@ -1,15 +1,30 @@
 // get entries from DOM
+const signupModal = document.getElementById('signupModal');
+const loginModal = document.getElementById('loginModal');
 const modalBody = document.querySelector('.modal-body');
 const signupForm = document.getElementById('signup-form');
+const loginForm = document.getElementById('login-form');
 const fullname = document.getElementById('fullname');
 const emailAdd = document.getElementById('email-add');
+const loginEmailAdd = document.getElementById('login-email-add');
 const phoneNo = document.getElementById('phone-no');
 const address = document.getElementById('address');
 const gender = document.getElementById('gender');
 const password = document.getElementById('password');
+const loginPassword = document.getElementById('login-password');
+const submit = document.getElementById('form-submit');
+const loginSubmit = document.getElementById('login-submit');
+const loaderIcon = document.querySelector(".fa-spin");
+const successMsg = document.querySelector(".success-message");
+const loginSuccessMsg = document.querySelector(".login-success-message");
 
 // store url paths
 const storeSignup = "https://jsminnastore.herokuapp.com/auth/signup";
+const storeLogin = "https://jsminnastore.herokuapp.com/auth/login/";
+
+const myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmdWxsTmFtZSI6ImhhcnJ5cG9ydGVyIDUiLCJpYXQiOjE2MDY0Nzk0MDUsImV4cCI6MTYwNjQ4MzAwNX0.V8j3bCZEMFiTa4yhpHdYP8uIesEU5ty6YzxNau2TKtE");
+myHeaders.append("Content-Type", "application/json");
 
 // run when page has loaded
 window.onload = () => {
@@ -20,9 +35,7 @@ window.onload = () => {
 
 	// signup form - new user registration
 	signupForm.onsubmit = () => {
-		const myHeaders = new Headers();
-		myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmdWxsTmFtZSI6ImhhcnJ5cG9ydGVyIDUiLCJpYXQiOjE2MDY0Nzk0MDUsImV4cCI6MTYwNjQ4MzAwNX0.V8j3bCZEMFiTa4yhpHdYP8uIesEU5ty6YzxNau2TKtE");
-		myHeaders.append("Content-Type", "application/json");
+		submit.innerHTML = `<i class="fa fa-spinner fa-spin mr-2"></i>Please Wait...`;
 
 		let raw = JSON.stringify({
 			"fullName":fullname.value,
@@ -43,11 +56,43 @@ window.onload = () => {
 		fetch(storeSignup, requestOptions)
 		  .then(response => response.text())
 		  .then(result => {
-		  	signupForm.style.display = 'none';
-		  	modalBody.innerHTML = `<h4 class="text-center text-success my-3">You have registered successfully!</h4>`
-			console.log(result)
+		  	signupForm.classList.add('d-none');
+		  	successMsg.classList.remove('d-none');
+	  	  	$('#signupModal').on('hidden.bs.modal', () => {
+	  	  		signupModal.reset();
+	  		});
+			console.log(result);
 		  })
 		  .catch(error => console.log('error', error));
-
 	}
+
+	// login form
+	loginForm.onsubmit = () => {
+		loginSubmit.innerHTML = `<i class="fa fa-spinner fa-spin mr-2"></i>Please Wait...`;
+
+		let raw = JSON.stringify({
+			"email":loginEmailAdd.value,
+			"password":loginPassword.value
+		});
+
+		const requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow'
+		};
+
+		fetch(storeLogin, requestOptions)
+		  .then(response => response.text())
+		  .then(result => {
+		  	loginForm.classList.add('d-none');
+		  	loginSuccessMsg.classList.remove('d-none');
+	  	  	$('#loginModal').on('hidden.bs.modal', () => {
+	  	  		loginForm.reset();
+	  		});
+			console.log(result);
+		  })
+		  .catch(error => console.log('error', error));
+	}
+	  	
 }
