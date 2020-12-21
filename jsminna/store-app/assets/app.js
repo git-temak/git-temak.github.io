ce9ce5714aaf48eee5ba2a2c2a3ca3b71430bcf5
Temak <makindeoluwatimilehin@gmail.com>
2020-12-21 15:27:20 +0100

    updates - bugs fixes
    - forms not working fix
    - form submissions fix
    - proper error handling

--- jsminna/store-app/assets/app.js
+++ jsminna/store-app/assets/app.js
@@ -35,116 +35,129 @@ myHeaders.append("Content-Type", "application/json");
 
 // run when page has loaded
 window.onload = () => {
-	// Initialise AOS
-	// if ( window.location.pathname === '/index.html' || window.location.pathname === '/'){
+	// signup form - new user registration
+	signupForm.onsubmit = (e) => {
+		e.preventDefault();
+		submit.innerHTML = `<i class="fa fa-spinner fa-spin mr-2"></i>Please Wait...`;
 
-		// signup form - new user registration
-		signupForm.onsubmit = () => {
-			submit.innerHTML = `<i class="fa fa-spinner fa-spin mr-2"></i>Please Wait...`;
+		let raw = JSON.stringify({
+			"fullName":fullname.value,
+			"email":emailAdd.value,
+			"mobileNumber":phoneNo.value,
+			"address":address.value,
+			"gender":gender.value,
+			"password":password.value
+		});
 
-			let raw = JSON.stringify({
-				"fullName":fullname.value,
-				"email":emailAdd.value,
-				"mobileNumber":phoneNo.value,
-				"address":address.value,
-				"gender":gender.value,
-				"password":password.value
-			});
+		const requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow'
+		};
 
-			const requestOptions = {
-				method: 'POST',
-				headers: myHeaders,
-				body: raw,
-				redirect: 'follow'
-			};
-
-			fetch(storeSignup, requestOptions)
-			  .then(response => response.text())
-			  .then(result => {
-			  	signupForm.classList.add('d-none');
-			  	successMsg.classList.remove('d-none');
-		  	  	$('#signupModal').on('hidden.bs.modal', () => {
-		  	  		signupModal.reset();
-		  		});
-				console.log(result);
-			  })
-			  .catch(error => {
-			  	$('.success-message h4').removeClass('text-success');
+		fetch(storeSignup, requestOptions)
+		  .then(response => response.text())
+		  .then(result => {
+		  	signupForm.classList.add('d-none');
+		  	successMsg.classList.remove('d-none');
+  	  		signupForm.reset();
+  	  		if (JSON.parse(result).success == false) {
 			  	$('.success-message h4').addClass('text-danger');
-			  	$('.success-message h4').innerText = error;
-			  	alert(error);
-			  	console.log('error', error);
-			  });
-		}
-
-		// login form
-		loginForm.onsubmit = () => {
-			loginSubmit.innerHTML = `<i class="fa fa-spinner fa-spin mr-2"></i>Please Wait...`;
-
-			let raw = JSON.stringify({
-				"email":loginEmailAdd.value,
-				"password":loginPassword.value
-			});
-
-			const requestOptions = {
-				method: 'POST',
-				headers: myHeaders,
-				body: raw,
-				redirect: 'follow'
-			};
-
-			fetch(storeLogin, requestOptions)
-			  .then(response => response.text())
-			  .then(result => {
-			  	loginForm.classList.add('d-none');
-			  	loginSuccessMsg.classList.remove('d-none');
-		  	  	$('#loginModal').on('hidden.bs.modal', () => {
-		  	  		loginForm.reset();
+			  	$('.success-message p').hide();
+			  	$('.success-message h4').text(JSON.parse(result).message);
+			} else {
+			  	$('.success-message h4').text('You have registered successfully!');
+			  	$('.success-message h4').addClass('text-success');
+		  	  	$('#signupModal').on('hidden.bs.modal', () => {
+		  	  		signupForm.classList.remove('d-none');
+				  	successMsg.classList.add('d-none');
+					submit.innerText = 'Sign up';
 		  		});
-				console.log(result);
-			  })
-			  .catch(error => {
-			  	$('.login-success-message h4').removeClass('text-success');
-			  	$('.login-success-message h4').addClass('text-danger');
-			  	$('.login-success-message h4').innerText = error;
-			  	alert(error);
-			  	console.log('error', error)
-			  });
-		}
-	// }
+			  }
+			console.log(result);
+		  })
+		  .catch(error => {
+		  	$('.success-message h4').removeClass('text-success');
+		  	$('.success-message h4').addClass('text-danger');
+		  	$('.success-message h4').innerText = error;
+		  	alert(error);
+		  	console.log('error', error);
+		  });
+	}
+
+	// login form
+	loginForm.onsubmit = (e) => {
+		e.preventDefault();
+		loginSubmit.innerHTML = `<i class="fa fa-spinner fa-spin mr-2"></i>Please Wait...`;
+
+		let raw = JSON.stringify({
+			"email":loginEmailAdd.value,
+			"password":loginPassword.value
+		});
+
+		const requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow'
+		};
+
+		fetch(storeLogin, requestOptions)
+		  .then(response => response.text())
+		  .then(result => {
+		  	loginForm.classList.add('d-none');
+		  	loginSuccessMsg.classList.remove('d-none');
+  	  		loginForm.reset();
+			console.log(result);
+			if (JSON.parse(result).success == false) {
+			  	$('.login-text-success').addClass('text-danger');
+			  	$('.login-success-message p').hide();
+			  	$('.login-text-success').text(JSON.parse(result).message);
+			} else {
+			  	$('.login-text-success').text('Login Successful!');
+			  	$('.login-text-success').addClass('text-success');
+			}
+		  })
+		  .catch(error => {
+		  	$('.login-text-success').addClass('text-danger');
+		  	$('.login-text-success').text(error);
+		  	console.log('error', error)
+		  });
+	}
 	
 	// if (window.location.pathname === '/suggest.html' ){
 		// suggest form
-		suggestForm.onsubmit = () => {
-			suggestSubmit.innerHTML = `<i class="fa fa-spinner fa-spin mr-2"></i>Please Wait...`;
+		// suggestForm.onsubmit = () => {
+		// 	suggestSubmit.innerHTML = `<i class="fa fa-spinner fa-spin mr-2"></i>Please Wait...`;
 
-			let raw = JSON.stringify({
-				"itemName":itemName.value,
-				"itemDescription":itemDescription.value,
-				"itemCategory":category.value,
-				"reason":reason.value
-			});
+		// 	let raw = JSON.stringify({
+		// 		"itemName":itemName.value,
+		// 		"itemDescription":itemDescription.value,
+		// 		"itemCategory":category.value,
+		// 		"reason":reason.value
+		// 	});
 
-			console.log(raw)
+		// 	console.log(raw)
 
-			const requestOptions = {
-				method: 'POST',
-				headers: myHeaders,
-				body: raw,
-				redirect: 'follow'
-			};
+		// 	const requestOptions = {
+		// 		method: 'POST',
+		// 		headers: myHeaders,
+		// 		body: raw,
+		// 		redirect: 'follow'
+		// 	};
 
-			fetch(storeSuggest, requestOptions)
-			  .then(response => response.text())
-			  .then(result => {
-			  	alert("Thank you!Your suggestion has been recorded successfully.")
-	  	  		loginForm.reset();
-				console.log(result);
-			  })
-			  .catch(error => {
-			  	alert(error);
-			  	console.log('error', error)
-			  });
-		}
+		// 	fetch(storeSuggest, requestOptions)
+		// 	  .then(response => response.text())
+		// 	  .then(result => {
+		// 	  	alert("Thank you!Your suggestion has been recorded successfully.")
+	 //  	  		loginForm.reset();
+		// 		console.log(result);
+		// 	  })
+		// 	  .catch(error => {
+		// 	  	alert(error);
+		// 	  	console.log('error', error)
+		// 	  });
+		// }
 	// }
 }
